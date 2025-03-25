4e2ddb535851daa0e512b6ff5c80be6f2b7026d5
lisadavis552-Git <lisadavis8479@gmail.com>
2025-03-25 13:40:57 +0000

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
-sudo apt update && sudo apt install nodejs npm
 
-# Stop any existing instance of the application 
-pm2 stop Before_app 
+# Update system and install nodejs and npm (consider using nvm for version management)
+sudo apt update && sudo apt install -y nodejs npm
+
+# Stop any existing instance of the application (make sure this is the correct pm2 name)
+pm2 stop Before_app || true  # Will not fail if the app isn't running
 
 # Change to the project directory
-cd BeforeApplication/
+cd BeforeApplication/ || { echo "Directory 'BeforeApplication' not found!"; exit 1; }
 
 # Install application dependencies
 npm install
@@ -13,8 +15,14 @@ npm install
 # Build the React application
 npm run build
 
-# Install serve globally to host the build
+# Install serve globally to serve the build files
 npm install -g serve
 
-# Start the React application using PM2
-pm2 start serve --name example_app -- -s build
+# Start the React app using PM2 (make sure pm2 is installed globally)
+pm2 start serve --name Before_Application -- -s build  # Serving the build directory on port 3000
+
+# Optionally save the PM2 process list (so it starts on system reboot)
+pm2 save
+
+# Optionally, configure PM2 to run on startup
+pm2 startup
